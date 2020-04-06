3ffa9d05ddd4f89755acaa07952532106a77e5cc
Mateus Fagundes <57625884+mateusfagundes@users.noreply.github.com>
2020-04-06 17:10:54 -0300

    Add files via upload

---
+++ src/horadosistema/HoraDoSistema.java
@@ -0,0 +1,14 @@
+package horadosistema;
+
+import java.util.Date;
+
+public class HoraDoSistema {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Date relogio = new Date();
+        System.out.println("Hora do sistema");
+        System.out.println(relogio.toString());
+    }
+    
+}
